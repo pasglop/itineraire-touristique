d0ef5760ebdda8c35396922ee8492c962fdf4820
david <david.delpuech@gmail.com>
2023-06-23 13:43:25 +0200

    Bug correction so that all tests passed

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -22,6 +22,6 @@ class TestLoadData:
     def test_data_folder_exists(self, loader):
         test = loader.unzip_data()
         assert test is True
-        assert os.path.isdir(loader.fullDirPath) is True
-        assert os.path.isfile(loader.fullDirPath + 'index') is True
+        assert os.path.isdir(loader.fullDirPath.rstrip('/')) is True
+        assert os.path.isfile(loader.fullDirPath + 'index.json') is True
         assert os.path.isdir(loader.fullDirPath + 'objects') is True
