06bd68ee5777683ae5a7b0767faa588c4e1e0fe1
marouen <ar.marouen@gmail.com>
2023-07-13 11:03:08 +0200

    ajout des tables restantes

--- source/data_processing/contacts_processing.py
+++ source/data_processing/contacts_processing.py
@@ -1,26 +1,21 @@
-from source.data_processing.table_processing import TableProcessing, ProcessError
-
-
-class ContactsProcessing(TableProcessing):
-    def __init__(self, data, db_session):
-        mapping = {
-            "id": "dc_identifier",
-            "places_id": "places_id",
-            "from": "opens",
-            "trough": "closes"
-        }
-        compare_keys = ["id"]
-        super().__init__('public.contacts', mapping, compare_keys, data, db_session)
-
-    def find_object(self):
-        """
-        This function is used to find the object in the database and check if it needs to be updated.
-        :return: boolean
-        """
-        return self.exists(f"places_id = '{self.data['places_id']}' AND opens = '{self.data['opens']}' AND closes = '{self.data['closes']}'")
-
-    def process(self, data):
-        # To make sure it is a valid contact entry, we need to check if it has places_id, opens, and closes fields
-        if data['places_id'] is None or data['opens'] is None or data['closes'] is None:
-            raise ProcessError("One or more required fields are missing from the contact entry")
-        super().process(data)
+from source.data_processing.table_processing import TableProcessing, ProcessError								
+								
+								
+class ContactsProcessing(TableProcessing):								
+    def __init__(self, data, db_session):								
+        mapping = {								
+            "places_id": "dc_identifier",								
+            "schema_url": 'hasContact/0/foaf_homepage/0',								
+            "type": 'hasContact/0/@type/1',								
+            "phone": 'hasContact/0/schema_telephone/0',								
+            "source_updated": "lastUpdateDatatourisme"								
+            }								
+        compare_keys = ["places_id", "source_updated"]								
+        super().__init__('contacts', mapping, compare_keys, data, db_session)								
+								
+								
+    def process(self, data):								
+ # To make sure it is a POI, we need to check dc_identifier								
+       if data['dc_identifier'] is None:								
+        raise ProcessError(f"Field dc_identifier is missing from the object')
+                          
